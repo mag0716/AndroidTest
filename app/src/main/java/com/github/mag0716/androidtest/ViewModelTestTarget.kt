14cfc5852a106b13a345697bce7dc316d232411d
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-10-26 13:21:15 +0900

    CoroutinesTestRule を作成

--- app/src/main/java/com/github/mag0716/androidtest/ViewModelTestTarget.kt
+++ app/src/main/java/com/github/mag0716/androidtest/ViewModelTestTarget.kt
@@ -7,10 +7,10 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class ViewModelTestTarget : ViewModel() {
-
     private val _data = MutableLiveData<String>()
     val data: LiveData<String> = _data
 
+    // viewModelScope で実行する処理内で suspend fun を使っている場合、完了を知る手段がないので Job を戻り値にする
     fun loadData(id: Int) = viewModelScope.launch {
         val data = CoroutinesTestTarget().loadData(id)
         _data.value = data
